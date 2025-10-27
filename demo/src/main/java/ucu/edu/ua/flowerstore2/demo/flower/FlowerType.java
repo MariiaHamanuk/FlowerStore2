a5f615ba5d2573f2a70af4d3b3e053cd6745fb85
MariiaHamaniuk <hamaniuk.pn@ucu.edu.ua>
2025-10-27 22:21:05 +0200

    corrected

--- demo/src/main/java/ucu/edu/ua/flowerstore2/demo/flower/FlowerType.java
+++
@@ -1,5 +0,0 @@
-package ucu.edu.ua.flowerstore2.demo.flower;
-
-public enum FlowerType {
-    CHAMOMILE, ROSE, TULIP, LILY;
-}
--- demo/src/main/java/ucu/edu/ua/flowerstore2/demo/payment/Payment.java
+++
@@ -1,4 +0,0 @@
-package ucu.edu.ua.flowerstore2.demo.payment;
-public interface Payment {
-    double pay(double p); 
-} 
